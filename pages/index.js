7d5303721553ba6dfcfc75c2bb089cf0c8f5046d
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-19 17:13:27 +0200

    add dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Lord of the Rings App</h1>
-      <p>
-        For an overview of the books please have a look at{" "}
-        <Link href="/volumes">Volumes</Link>.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Lord of the Rings App</title>
+      </Head>
+      <div>
+        <h1>Lord of the Rings App</h1>
+        <p>
+          For an overview of the books please have a look at{" "}
+          <Link href="/volumes">Volumes</Link>.
+        </p>
+      </div>
+    </>
   );
 }
--- pages/volumes/index.js
+++ pages/volumes/index.js
@@ -1,26 +1,24 @@
 import { introduction } from "../../lib/data";
 import Link from "next/link";
+import { volumes } from "../../lib/data";
+import Head from "next/head";
 
 export default function Volumes() {
   return (
     <>
+      <Head>
+        <title>Lord of the Rings</title>
+      </Head>
       <h1>Lord of the Rings</h1>
       <h2>All Volumes</h2>
       <ul>
-        <li>
-          <Link href="/volumes/the-fellowship-of-the-ring">
-            The Fellowship of the Ring
-          </Link>
-        </li>
-        <li>
-          {" "}
-          <Link href="/volumes/the-two-towers">The Two Towers</Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-return-of-the-king">
-            The Return of the King
-          </Link>
-        </li>
+        {volumes.map((volume) => {
+          return (
+            <li key={volume.slug}>
+              <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
