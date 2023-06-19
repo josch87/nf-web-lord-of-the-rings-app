c02e040256a961e9f7feebc9d29560ee277f10ed
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-19 11:56:57 +0200

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Lord of the Rings App</h1>
+      <p>
+        For an overview of the books please have a look at{" "}
+        <Link href="/volumes">Volumes</Link>.
+      </p>
     </div>
   );
 }
