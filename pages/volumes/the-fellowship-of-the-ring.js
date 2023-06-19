7d2fdfa5fc33b15b2da4d29ef16e0897541ad4bb
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-19 11:54:33 +0200

    add volume covers

--- pages/volumes/the-fellowship-of-the-ring.js
+++ pages/volumes/the-fellowship-of-the-ring.js
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import Image from "next/image";
 import { volumes } from "../../lib/data";
 import { Fragment } from "react";
 
-export default function TheFellowshopOfTheRings() {
+export default function TheFellowshipOfTheRing() {
   const volumeData = volumes.find(
     ({ slug }) => slug === "the-fellowship-of-the-ring"
   );
@@ -12,7 +13,6 @@ export default function TheFellowshopOfTheRings() {
       <Link href="/volumes">← All Volumes</Link>
       <h1>{volumeData.title}</h1>
       <p>{volumeData.description}</p>
-
       <ul>
         {volumeData.books.map((book) => {
           return (
@@ -24,6 +24,16 @@ export default function TheFellowshopOfTheRings() {
           );
         })}
       </ul>
+      <Image
+        src="/images/the-fellowship-of-the-ring.png"
+        height={931}
+        width={574}
+        alt="The Fellowship of the Ring"
+      />
+      <br />
+      <Link href="/volumes/the-two-towers" title="The Two Towers">
+        Next
+      </Link>
     </>
   );
 }
